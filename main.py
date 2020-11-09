c33da2f737ab3ccb23f10436d2cb8667e95cb158
Sawyer Alec Cherry <42384257+SawyerCherry@users.noreply.github.com>
2020-11-09 11:15:08 -0600

    created the class

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 # Some Code
+
+class BankAccount():
+    def __init__(self, full_name, account_number, routing_number, balance):
+        self.full_name = full_name
+        self.account_number = account_number
+        self.routing_number = routing_number
+        
+
+
+    
