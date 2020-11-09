bc2da822c857fe45b14f0621c5086e7a6f4e6a41
Sawyer Alec Cherry <42384257+SawyerCherry@users.noreply.github.com>
2020-11-09 11:32:13 -0600

    built the methods in the class, Now I need to build a method to generate account numbers.

--- main.py
+++ main.py
@@ -5,7 +5,41 @@ class BankAccount():
         self.full_name = full_name
         self.account_number = account_number
         self.routing_number = routing_number
+        self.balance = balance
+
+
+    def deposit(self, amount):
+        # originally 0 but change it when you deposit amount
+        self.balance = self.balance + amount 
+
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            #this should charge you 10 dollars when you overdraft #thanksAsIfIWeren'tAlreadyBroke
+            self.balance -= amount
+        else:
+            self.balance = amount - 10
+
+            #prints out a message
+
+            print(f"{full_name}, you have Insufficient Funds, we have charged you $10 for an overdraft fee. You're  Welcome!")
+
+    def print_receipt(self, full_name, account_number, routing_number, balance):
+        return self.balance
+
+
+
         
+            
+
+    
+            
+        
+
+
+
+
+
+
 
 
     
